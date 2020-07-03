9ed5d95eddb2cb176ed2ffcde034374019169d1f
Sean Xiang Gao <gxiang1228@gmail.com>
2020-07-03 14:12:57 -0700

    Update todo.py

--- src/todo.py
+++ src/todo.py
@@ -1,8 +1,9 @@
 import torch
 
-def pick_tokens(prob, k):
+def pick_tokens(prob):
     # prob:     tensor, shape = [n, vocab_size], the predicted token generation probablity
     # return:   tensor, shape = [n, k], picked token index based on prob
+    #           where k is a hyperparameter you can choose
 
     # please implement your algorithm here
     pass
