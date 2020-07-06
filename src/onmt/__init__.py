514ac18b1b04528f2a21fa70e259977f2e21de4d
Sean Xiang Gao <gxiang1228@gmail.com>
2020-07-05 20:29:41 -0700

    + onmt

---
+++ src/onmt/__init__.py
@@ -0,0 +1,12 @@
+import onmt.Constants
+import onmt.Models
+from onmt.Translator import Translator as Translator
+from onmt.Dataset import Dataset
+from onmt.Optim import Optim
+from onmt.Dict import Dict
+from onmt.Beam import Beam
+
+""" 
+commit https://github.com/shrimai/Towards-Content-Transfer-through-Grounded-Text-Generation/commit/dd2246f557c39aee69711aabc1e3540a7c9f27a7
+some bugs-fixing/modification by Xiang Gao @ Microsoft Research
+"""
